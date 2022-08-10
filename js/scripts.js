0bb6bbbb9078f1d8529573f4b6a69c06a804c7b3
tgold83 <ted.jonathan.gold@gmail.com>
2022-08-10 09:38:07 -0700

    add HTML and JS into the folders

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+  // new line below
+  console.log("Script executing!");
+
+  let form = document.querySelector("form");
+  form.onsubmit = function(event) {
+    // new line below
+    console.log("Submit form successfully reached.");
+    const person1Input = document.getElementById("person1Input").value;
+    const person2Input = document.getElementById("person2Input").value;
+    const animalInput= document.getElementById("animalInput").value;
+    const exclamationInput = document.getElementById("exclamationInput").value;
+    const verbInput = document.getElementById("verbInput").vlaue;
+    const nounInput = document.getElementById("nounInput").value;
+
+    document.querySelector("span#person1a").innerText = person1Input;
+    document.querySelector("span#person1b").innerText = person1Input;
+    document.querySelector("span#person1c").innerText = person1Input;
+    document.querySelector("span#person2a").innerText = person2Input;
+    document.querySelector("span#person2b").innerText = person2Input;
+    document.querySelector("span#animal").innerText = animalInput;
+    document.querySelector("span#verb").innerText = verbInput;
+    document.querySelector("span#noun").innerText = nounInput;
+    document.querySelector("span#exclmation").innerText = exclamationInput;
+
+    document.querySelector("div#story").removeAttribute("class");
+  };
+};
